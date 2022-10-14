82d0872081a01a90acaa33a169e2b4a7b36c1325
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-10-14 13:37:21 -0400

    Arrow key navigation

--- src/ManyTimePad.js
+++ src/ManyTimePad.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./ManyTimePad.css";
 
 const toCells = (data) => data.map((d, i) => <td key={i}>{d}</td>);
@@ -6,13 +6,42 @@ const changeIndex = (array, i, value) => array.map((a, j) => j === i ? value : a
 const change2DIndex = (array, i, j, value) => {
     return array.map((a, k) => a.map((b, l) => i === k && j === l ? value : b));
 };
+const clamp = (value, min, max) => value < min ? min : (value > max ? max : value);
 
 function ManyTimePad({ encrypted, messageLength }) {
+    const [focus, setFocus] = useState([0, 0]);
     const [guesses, setGuesses] = useState(Array.from(Array(encrypted.length), () => Array.from(Array(messageLength), () => '')));
     const [openCols, setOpenCols] = useState(Array.from(Array(messageLength), () => [true, 0]));
+    const focusRef = useRef(null);
+
+    useEffect(() => {
+        if (focusRef.current)
+            focusRef.current.focus();
+    }, [focus]);
+
+    const handleKeys = (i, j) => (e) => {
+        switch (e.keyCode) {
+            case 37: // Left
+                setFocus([i, clamp(j - 1, 0, messageLength - 1)]);
+                break;
+            case 38: // Up
+                setFocus([clamp(i - 1, 0, encrypted.length - 1), j]);
+                break;
+            case 39: // Right
+                setFocus([i, clamp(j + 1, 0, messageLength - 1)]);
+                break;
+            case 40: // Down
+                setFocus([clamp(i + 1, 0, encrypted.length - 1), j]);
+                break;
+            default:
+                break;
+        }
+    };
+
     const guess = (i, j) => (e) => {
         const char = e.target.value.substring(e.target.value.length - 1);
-        console.log(char);
+        if (char)
+            setFocus([i, clamp(j + 1, 0, messageLength)]);
         setGuesses(change2DIndex(guesses, i, j, char));
         setOpenCols(changeIndex(openCols, j, [!char, i]));
     };
@@ -28,7 +57,10 @@ function ManyTimePad({ encrypted, messageLength }) {
             message >>= 8n;
 
             const disabled = !openCols[j][0] && openCols[j][1] !== i;
-            guessInputs.unshift(<input type="text" onChange={guess(i, j)} value={guesses[i][j]} disabled={disabled}/>)
+            const focused = i === focus[0] && j === focus[1];
+            guessInputs.unshift(
+                <input type="text" ref={focused ? focusRef : undefined} onChange={guess(i, j)} onKeyDown={handleKeys(i, j)} value={guesses[i][j]} disabled={disabled}/>
+            );
         }
 
         rows.push(<tr key={i}>{toCells(messageChars)}</tr>);
