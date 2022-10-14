eeb3beac4e3c0c957e15ed988d436ea06ba833dc
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-10-14 14:26:12 -0400

    Column decryption

--- src/ManyTimePad.js
+++ src/ManyTimePad.js
@@ -7,6 +7,8 @@ const change2DIndex = (array, i, j, value) => {
     return array.map((a, k) => a.map((b, l) => i === k && j === l ? value : b));
 };
 const clamp = (value, min, max) => value < min ? min : (value > max ? max : value);
+const oToC = (ord) => String.fromCharCode(Number(ord));
+const cToO = (char) => BigInt(char.charCodeAt(0));
 
 function ManyTimePad({ encrypted, messageLength }) {
     const [focus, setFocus] = useState([0, 0]);
@@ -15,13 +17,19 @@ function ManyTimePad({ encrypted, messageLength }) {
     const focusRef = useRef(null);
 
     useEffect(() => {
-        if (focusRef.current)
+        if (focusRef.current) {
+            focusRef.current.setSelectionRange(1, 1);
             focusRef.current.focus();
+        }
     }, [focus]);
 
     const handleKeys = (i, j) => (e) => {
         switch (e.keyCode) {
+            case 8: // Backspace
+                if (!e.target || e.target.value)
+                    break
             case 37: // Left
+                e.preventDefault();
                 setFocus([i, clamp(j - 1, 0, messageLength - 1)]);
                 break;
             case 38: // Up
@@ -53,13 +61,19 @@ function ManyTimePad({ encrypted, messageLength }) {
         let message = encrypted[i];
 
         for (let j = messageLength - 1; j >= 0; j--) {
-            messageChars.unshift(String.fromCharCode(Number(message & 255n)));
+            const char = message & 255n;
+            messageChars.unshift(oToC(char));
             message >>= 8n;
 
             const disabled = !openCols[j][0] && openCols[j][1] !== i;
+            let decrypt;
+            if (disabled) 
+                decrypt = cToO(guesses[openCols[j][1]][j]) ^ ((encrypted[openCols[j][1]] >> (8n * BigInt(i))) & 255n) ^ char;
+            const value = disabled ? oToC(decrypt) : guesses[i][j];
             const focused = i === focus[0] && j === focus[1];
             guessInputs.unshift(
-                <input type="text" ref={focused ? focusRef : undefined} onChange={guess(i, j)} onKeyDown={handleKeys(i, j)} value={guesses[i][j]} disabled={disabled}/>
+                <input type="text" ref={focused ? focusRef : undefined} onChange={guess(i, j)}
+                       onKeyDown={handleKeys(i, j)} value={value} disabled={disabled}/>
             );
         }
 
